e4e254460d62120b4f3484b02cc51f38de76ed3c
Mauricio Muriel <mauricio.muriel@calitek.net>
2017-08-25 20:26:15 -0500

    Integrando el API de Shiphero y de Google Sheets v4, generando los dos comandos iniciales para probar los servicios... conexiones OK

--- app/app/Console/Kernel.php
+++ app/app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        Commands\ShipheroApiGetPOCommand::Class,
+        Commands\GoogleSpreadSheetApiCommand::Class,
     ];
 
     /**
